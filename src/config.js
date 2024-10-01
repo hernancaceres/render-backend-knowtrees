e637e1d7125ca7320c915a149f5b98abf79e96a2
hernancaceres <harinatha777@hotmail.com>
2024-10-01 09:30:22 -0300

    backend corriendo en local actualizacion

---
+++ src/config.js
@@ -0,0 +1,12 @@
+// config.js
+import dotenv from "dotenv";
+dotenv.config();
+
+
+export const FRONTEND_URL = process.env.FRONTEND_URL 
+export const DB_HOST = process.env.DB_HOST 
+export const DB_PORT = process.env.DB_PORT 
+export const DB_DATABASE = process.env.DB_DATABASE 
+export const DB_USER = process.env.DB_USER 
+export const DB_PASSWORD = process.env.DB_PASSWORD 
+export const PORT = process.env.PORT 
